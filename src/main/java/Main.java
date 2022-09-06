6dc3329d1d2b44673423d3296372375f081e66e5
amieldelatorre <amieljames.delatorre@gmail.com>
2022-09-06 13:13:19 +1200

    Retrieve the word to guess by player 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a word to guess: ");
+        String wordToGuess = scanner.nextLine();
+        if (wordToGuess.isEmpty() || wordToGuess.isBlank())
+            System.out.println("Empty String");
+        else
+            System.out.println(wordToGuess);
     }
 }
