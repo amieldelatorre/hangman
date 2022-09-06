ffd4fb0748c31e48c7b693040c7e6fb253fa3b77
amieldelatorre <amieljames.delatorre@gmail.com>
2022-09-06 13:15:24 +1200

    Edit: wording when retrieving word to guess

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a word to guess: ");
+        System.out.println("Player 1, please enter a word to guess for player 2: ");
         String wordToGuess = scanner.nextLine();
         if (wordToGuess.isEmpty() || wordToGuess.isBlank())
             System.out.println("Empty String");
