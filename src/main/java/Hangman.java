bb1e365070197c4cf57cea3528464caaf708421a
amieldelatorre <amieljames.delatorre@gmail.com>
2022-09-20 20:41:32 +1200

    Adding getting the number of incorrect/correct guesses and guess function

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,15 +1,63 @@
-public class Hangman {
-    String wordToGuess;
-    int mistakesAllowed;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-    public static final int defaultMistakesAllowed = 5;
+public class Hangman {
+    private String wordToGuess;
+    int mistakesAllowed;
+    List<String> correctGuesses = new ArrayList<String>();
+    List<String> incorrectGuesses = new ArrayList<String>();
+
+    public static final int DEFAULT_MISTAKES_ALLOWED = 5;
 
     public Hangman(String wordToGuess) {
-        this(wordToGuess, defaultMistakesAllowed);
+        this(wordToGuess, DEFAULT_MISTAKES_ALLOWED);
     }
 
     public Hangman(String wordToGuess, int mistakesAllowed) {
         this.wordToGuess = wordToGuess;
         this.mistakesAllowed = mistakesAllowed;
+    }
+
+    public String getWordToGuess() {
+        return wordToGuess;
+    }
+
+    public int getNumberOfCorrectGuesses() {
+        return correctGuesses.size();
+    }
+
+    public int getNumberOfIncorrectGuesses() {
+        return incorrectGuesses.size();
+    }
+
+    public int getNumberOfGuesses() {
+        return getNumberOfCorrectGuesses() + getNumberOfIncorrectGuesses();
+    }
+
+    public boolean guess(String guessAttempt) {
+        boolean guessResult;
+        if (guessAttempt.length() > 1)
+            guessResult = guessWord(guessAttempt);
+        else
+            guessResult = guessLetter(guessAttempt);
+
+        if (guessResult)
+            correctGuesses.add(guessAttempt);
+        else
+            incorrectGuesses.add(guessAttempt);
+
+        return guessResult;
+    }
+
+    public boolean guessWord(String guessWord) {
+        return wordToGuess.equals(guessWord);
+    }
+
+    public boolean guessLetter(String guessChar) {
+        int indexOfLetter = wordToGuess.indexOf(guessChar);
+        // indexOf returns -1 if string or character is not found
+        return indexOfLetter != -1;
     }
 }
