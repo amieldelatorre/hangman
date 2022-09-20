e7536adb615a887a845bdac1d037d52b0f23acff
amieldelatorre <amieljames.delatorre@gmail.com>
2022-09-20 20:43:17 +1200

    Implementing game in Main, incomplete, while loop, left to flesh it out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
+import java.io.Console;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Console console = System.console();
 
         System.out.println("Player 1, please enter a word to guess for player 2: ");
-        String wordToGuess = scanner.nextLine();
+        char[] charsToGuess;
+        if (console == null)
+            charsToGuess = scanner.nextLine().toCharArray();
+        else
+            charsToGuess = console.readPassword();
+        String wordToGuess = String.valueOf(charsToGuess);
         if (wordToGuess.isEmpty() || wordToGuess.isBlank()) {
             System.out.println("Input was empty, terminating program!");
             System.exit(0);
@@ -14,5 +21,13 @@ public class Main {
             System.out.println(wordToGuess);
 
         Hangman game = new Hangman(wordToGuess);
+
+        String guessWord;
+        boolean guessedCorrectly = false;
+        while (game.getNumberOfIncorrectGuesses() < Hangman.DEFAULT_MISTAKES_ALLOWED && !guessedCorrectly) {
+            System.out.println("Player 2, please guess a letter or a word: ");
+            guessWord = scanner.nextLine();
+            guessedCorrectly = game.guess(guessWord);
+        }
     }
 }
